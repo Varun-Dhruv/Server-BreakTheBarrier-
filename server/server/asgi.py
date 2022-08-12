23f0c71403ad9399bbdb0d5e8a682a928f84197f
Varun Koranne <88835482+Varun-Dhruv@users.noreply.github.com>
2022-08-12 23:29:37 +0530

    added consumers

--- server/server/asgi.py
+++ server/server/asgi.py
@@ -7,14 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
+
+from django.urls import path, include
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from SignLanguageDetection.consumers import *
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
+ws_patterns=[
+    path('ws/test', TestConsumer.as_asgi())
+]
+
 application=ProtocolTypeRouter({
+    'websocket':URLRouter(ws_patterns),    
     'http': get_asgi_application(),
 })
-
